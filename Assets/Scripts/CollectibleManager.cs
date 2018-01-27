0c6b85a5e60e5e23feded72fda98ee78d20e0d8e
Spencer Shook <sscheckmate1@gmail.com>
2018-01-27 16:07:10 -0500

    Initial Commit
    1/16/2018 Initial Commit

---
+++ Assets/Scripts/CollectibleManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets._2D;
+using UnityStandardAssets.CrossPlatformInput;
+
+[RequireComponent(typeof(PlatformerCharacter2D))]
+public class CollectibleManager : MonoBehaviour {
+
+    public float smallHealAmount;
+    private PlatformerCharacter2D platChar;
+
+    void Start()
+    {
+        platChar = GetComponent<PlatformerCharacter2D>();
+    }
+	void OnTriggerEnter2D(Collider2D col)
+    {
+        string tag = col.gameObject.tag;
+        if (tag == "SmallHeal")
+        {
+            platChar.healDamage(smallHealAmount);
+            col.gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityStandardAssets._2D;
+public class UIController : MonoBehaviour {
+
+    public Text HealthText;
+    public Text KurenCText;
+    public PlatformerCharacter2D pc2d;
+    public Slider HealthSlider;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        HealthText.text = "Health: " + pc2d.getHealth();
+        KurenCText.text = "Kuren C's: " + pc2d.getKurenCs();
+        HealthSlider.value = pc2d.getHealth() / pc2d.getMaxHealth();
+	}
+}
